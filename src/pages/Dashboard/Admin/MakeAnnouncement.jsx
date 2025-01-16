86bcbec162c32869cef9a1881358f3cce8878c43
Arman <mdarman365506@gmail.com>
2025-01-16 15:28:05 +0600

    uploaded data on database for announcement

--- src/pages/Dashboard/Admin/MakeAnnouncement.jsx
+++ src/pages/Dashboard/Admin/MakeAnnouncement.jsx
@@ -1,10 +1,60 @@
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 
 
 const MakeAnnouncement = () => {
+    const axiosSecure = useAxiosSecure();
+
+    const handleSubmitForm = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const title = form.title.value;
+        const description = form.description.value;
+        // console.log(title, description);
+        
+        const announcement = {
+            title,
+            description,
+        }
+
+        axiosSecure.post("/announcements", announcement)
+            .then(res => {
+            console.log('announcement upload successfully', res.data);
+            })
+            .catch(errro => {
+            console.log('announcement uploaded failed', errro);
+        })
+    }
     return (
-        <div>
-            make announcement ...
-        </div>
+      <div>
+        make announcement ...
+        <form onSubmit={handleSubmitForm} className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Title</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Title"
+              name="title"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Description</span>
+            </label>
+            <textarea
+              className="textarea textarea-bordered resize-none"
+                        placeholder="description"
+                        name="description"
+            ></textarea>
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary">Submit</button>
+          </div>
+        </form>
+      </div>
     );
 };
 
