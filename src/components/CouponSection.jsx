eedb17dc5a57729c5cd618d8de7793e9489c0372
Arman <mdarman365506@gmail.com>
2025-01-19 23:48:02 +0600

    added discount image on coupons section

--- src/components/CouponSection.jsx
+++ src/components/CouponSection.jsx
@@ -1,11 +1,13 @@
 import { toast } from "react-toastify";
 import Loading from "../pages/Loading";
 import Coupons from "../Shared/Coupons";
+import SectionTitle from "../Shared/SectionTitle";
+import Button from "../Shared/Button";
+import notFoundImg from '../../src/assets/not-found.png'
 
 const CouponSection = () => {
   const [data, isLoading, refetch] = Coupons();
   if (isLoading) return <Loading />;
-
   const handleCopyCode = (couponCode) => {
     navigator.clipboard
       .writeText(couponCode)
@@ -21,26 +23,27 @@ const CouponSection = () => {
 
   return (
     <div>
-      coupon section ...{data.length}
+      <SectionTitle
+        heading={`Exclusive Offers`}
+        subHeading={`Grab exciting discounts with our special coupons. Save big and enjoy more—don’t miss out!`}
+      />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
         {data.map((coupon) => (
           <div key={coupon._id} className="card bg-base-100  shadow-xl">
             <figure className="px-10 pt-10">
               <img
-                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                alt="Shoes"
-                className="rounded-xl"
+                src={coupon.image || notFoundImg}
+                onError={(e) => e.target.src=notFoundImg}
+                alt="Discount"
+                className="rounded-full w-36 h-36"
               />
             </figure>
             <div className="card-body items-center text-center">
               <h2 className="card-title">Discount: {coupon.percentage} %</h2>
               <p>{coupon.description}</p>
               <div className="card-actions">
-                <button
-                  className="btn btn-primary"
-                  onClick={() => handleCopyCode(coupon.code)}
-                >
-                  Copy Code
+                <button onClick={() => handleCopyCode(coupon.code)}>
+                  <Button styleBtn={`Copy Code`} />
                 </button>
               </div>
             </div>
