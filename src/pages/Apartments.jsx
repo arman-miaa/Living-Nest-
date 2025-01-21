df1dd55f7369cfab2be71827230065369ce9adb2
Arman <mdarman365506@gmail.com>
2025-01-21 08:38:45 +0600

    apartments page design done

--- src/pages/Apartments.jsx
+++ src/pages/Apartments.jsx
@@ -2,13 +2,15 @@ import { useQuery } from "@tanstack/react-query";
 import useAxiosPublic from "../Hooks/useAxiosPublic";
 import useAuth from "../Hooks/useAuth";
 import { toast } from "react-toastify";
-import { useEffect, useState } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import useAxiosSecure from "../Hooks/useAxiosSecure";
 import useRole from "../Hooks/useRole";
 import Loading from "./Loading";
-import notFoundImg from '../../src/assets/not-found.png'
+import notFoundImg from "../../src/assets/not-found.png";
 
+
+import { FaBuilding,  FaLayerGroup, FaDoorOpen, FaCoins } from "react-icons/fa";
 import Button from "../Shared/Button";
 
 const Apartments = () => {
@@ -19,29 +21,25 @@ const Apartments = () => {
   const [currentPage, setCurrentPage] = useState(0);
   const [itemsPerPage, setItemsPerPage] = useState(6);
   const [count, setCount] = useState(0);
-  const [minRent,setMinRent] = useState(0)
-  const [maxRent,setMaxRent] = useState(0)
+  const [minRent, setMinRent] = useState(0);
+  const [maxRent, setMaxRent] = useState(0);
   const navigate = useNavigate();
 
- 
-
-  // Calculate the number of pages
   const numberOfPages = Math.ceil(count / itemsPerPage);
   const pages = [...Array(numberOfPages).keys()];
 
-  // Get apartments data with pagination
   const {
     data = [],
     isLoading: loading,
     error,
   } = useQuery({
-    queryKey: ["apartments", currentPage, itemsPerPage,minRent,maxRent],
+    queryKey: ["apartments", currentPage, itemsPerPage, minRent, maxRent],
     queryFn: async () => {
       const response = await axiosPublic.get(
         `/apartments?page=${currentPage}&limit=${itemsPerPage}&minRent=${minRent}&maxRent=${maxRent}`
       );
-      setCount(response.data.total); // Set the total count from the server response
-      return response.data.apartments; // Apartments data
+      setCount(response.data.total);
+      return response.data.apartments;
     },
   });
 
@@ -69,68 +67,74 @@ const Apartments = () => {
       axiosSecure
         .post("/agreements", agreementData)
         .then((res) => {
-          console.log(res.data);
+          toast.success("Agreement successfully created!");
         })
         .catch((error) => {
-          console.log("ERROR", error.response.data.message);
           toast.warn(error.response.data.message);
         });
-
-      console.log("agreement data", agreementData);
     } else {
       navigate("/login");
     }
   };
 
-const handleSearchFilter = (e) => {
-  e.preventDefault();
-  const form = e.target;
-  const min = parseInt(form.minRent.value, 10);
-  const max = parseInt(form.maxRent.value, 10);
+  const handleSearchFilter = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const min = parseInt(form.minRent.value, 10);
+    const max = parseInt(form.maxRent.value, 10);
 
-  if (min > max) {
-    toast.error("Min Rent cannot be greater than Max Rent");
-    return;
-  }
+    if (min > max) {
+      toast.error("Min Rent cannot be greater than Max Rent");
+      return;
+    }
 
-  setMinRent(min);
-  setMaxRent(max);
-  setCurrentPage(0); // Reset pagination to the first page
-};
-
+    setMinRent(min);
+    setMaxRent(max);
+    setCurrentPage(0);
+  };
 
   return (
-    <div>
+    <div className="p-6">
       {/* Search */}
-      <div className="flex  justify-center py-8 items-center gap-2">
-        <form onSubmit={handleSearchFilter} action="">
+      <div className="flex flex-wrap justify-center items-centergap-4 mb-6  md:p-4 rounded-md ">
+        <form
+          onSubmit={handleSearchFilter}
+          className="flex flex-col md:flex-row gap-4"
+        >
           <div className="form-control">
             <label className="label">
-              <span className={`label-text font-semibold `}>MinRent</span>
+              <span className="label-text font-semibold text-accent text-xl">
+                Min Rent
+              </span>
             </label>
             <input
               type="number"
               name="minRent"
-              placeholder="MinRent"
-              className={`input  border-emerald-700 bg-transparent input-bordered focus:outline-none focus:ring-2 `}
+              placeholder="Min Rent"
+              className="input input-bordered w-full"
               required
             />
           </div>
           <div className="form-control">
             <label className="label">
-              <span className={`label-text font-semibold `}>MinRent</span>
+              <span className="label-text font-semibold -mt-4 md:-mt-0 text-accent text-xl">
+                Max Rent
+              </span>
             </label>
             <input
               type="number"
               name="maxRent"
-              placeholder="MaxRent"
-              className={`input  border-emerald-700 bg-transparent input-bordered focus:outline-none focus:ring-2 `}
+              placeholder="Max Rent"
+              className="input input-bordered w-full"
               required
             />
           </div>
-          <button className="btn btn-primary mt-8">Search</button>
+          <button type="submit" className="btn bg-secondary text-white hover:bg-orange-700 self-center md:mt-10">
+            Search
+          </button>
         </form>
       </div>
+
       {/* Apartments Grid */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {data.length === 0 ? (
@@ -141,46 +145,65 @@ const handleSearchFilter = (e) => {
           data.map((apartment) => (
             <div
               key={apartment._id}
-              className="flex flex-col h-full bg-base-100 shadow-xl"
+              className="flex flex-col h-full bg-base-100 shadow-lg rounded-lg overflow-hidden"
             >
               <figure>
                 <img
                   src={apartment.image || notFoundImg}
                   alt="Apartment"
-                  className="w-full h-[200px] object-cover rounded-t-xl"
+                  className="w-full h-[200px] object-cover"
                   onError={(e) => (e.target.src = notFoundImg)}
                 />
               </figure>
-              <div className="flex flex-col flex-grow mt-2 mx-6">
-                <h2 className="text-2xl lg:text-3xl font-semibold text-secondary">
+              <div className="p-4 flex flex-col h-full">
+                <h2 className="text-xl font-semibold text-secondary mb-2">
                   {apartment.title}
                 </h2>
-                <p className="mt-2 text-justify">{apartment.description}</p>
+                <p className="text-sm text-gray-600 mb-4">
+                  {apartment.description}
+                </p>
+
                 <div className="flex justify-between mt-2 mb-4">
-                  <div>
-                    <p>Floor No: {apartment.floorNo}</p>
-                    <p>Block Name: {apartment.blockName}</p>
+                  <div className="flex flex-col gap-2">
+                    <p>
+                      <FaLayerGroup className="inline-block text-primary" />{" "}
+                      Floor No: {apartment.floorNo}
+                    </p>
+                    <p>
+                      <FaBuilding className="inline-block text-secondary" />{" "}
+                      Block Name: {apartment.blockName}
+                    </p>
                   </div>
-                  <div>
-                    <p>Apartment No: {apartment.apartmentNo}</p>
-                    <p>Rent: {apartment.rent}</p>
+                  <div className="flex flex-col gap-2">
+                    <p>
+                      <FaDoorOpen className="inline-block text-info" />{" "}
+                      Apartment No: {apartment.apartmentNo}
+                    </p>
+                    <p>
+                      <FaCoins className="inline-block text-warning" /> Rent:{" "}
+                      {apartment.rent}$
+                    </p>
                   </div>
                 </div>
-                <div className="mt-auto mb-4">
-                  {apartment.availability === "available" ? (
-                    <div
-                      onClick={() => handleAgreement(apartment)}
-                     className="w-full"
-                    >
-                     
-                      <Button styleBtn={`Agreement`}/>
-                    </div>
-                  ) : (
-                    <span className="text-gray-600 font-semibold border-2 bg-orange-300 p-2 mt-2 block text-center">
-                      Already Rented
-                    </span>
-                  )}
-                </div>
+                {role === "admin" ? (
+                  <span className="text-white mt-auto bg-accent text-center py-2 block rounded-md">
+                    Admin Panel - Manage Apartment
+                  </span>
+                ) : (
+                  <div className="mt-auto">
+                    {apartment.availability === "available" ? (
+                      <Button
+                        onClick={() => handleAgreement(apartment)}
+                        styleBtn="Agreement"
+                        width="w-full"
+                      />
+                    ) : (
+                      <span className="text-gray-500 bg-red-200 text-center py-2 block rounded-md">
+                        Already Rented
+                      </span>
+                    )}
+                  </div>
+                )}
               </div>
             </div>
           ))
@@ -188,14 +211,14 @@ const handleSearchFilter = (e) => {
       </div>
 
       {/* Pagination */}
-      <div className="flex justify-center mt-4">
+      <div className="flex justify-center mt-6">
         <div className="btn-group">
           {pages.map((page) => (
             <button
               key={page}
               onClick={() => setCurrentPage(page)}
-              className={`btn ${
-                currentPage === page ? "btn-primary" : "btn-outline"
+              className={`btn mr-2 border-orange-600 ${
+                currentPage === page ? "btn-primary bg-accent" : "btn-outline"
               }`}
             >
               {page + 1}
